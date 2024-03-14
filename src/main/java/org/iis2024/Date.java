96b5bdc6a0ac50184e6ca26fcb1c42f652d988d6
lcc <lcc@lcc.lcc.es>
2024-03-14 13:45:12 +0100

    update class Date

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -55,6 +55,7 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
+  private int nnumero;
   public Date(int day, int month, int year) {
     this.day = day;
     this.month = month;
